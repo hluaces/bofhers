e8cb06bc56ddb842dd482b9857c6d7a9d071286d
Héctor Luaces <hector@luaces-novo.es>
2020-11-13 00:10:23 +0100

    fix: renaming 'tag' for 'category'

--- app/Console/Commands/sendQuote.php
+++ app/Console/Commands/sendQuote.php
@@ -17,19 +17,20 @@ class sendQuote extends Command
      *
      * @var string
      */
-    protected $signature = 'telegram:sendquote {canal?} {tag?}';
+    protected $signature = 'telegram:sendquote {canal?} {category?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Send random quote to a channel (tag is optional)';
+    protected $description = 'Send random quote to a channel (category is optional)';
 
     /**
      * Create a new command instance.
      *
      * @return void
+     * @throws \Telegram\Bot\Exceptions\TelegramSDKException
      */
     public function __construct()
     {
@@ -48,14 +49,16 @@ class sendQuote extends Command
     public function handle()
     {
         $canal = $this->argument('canal');
-        $tag = $this->argument('tag');
+        $category = $this->argument('category');
         $canales = TelegramCanal::where('active', 1)->get();
         if($canal) {
             $canales = TelegramCanal::where('name', "#".$canal)->where('active', 1)->get();
         }
 
         foreach($canales as $canal){
-            $quote = Quote::getAndMarkRandomQuoteText($canal->chat_id, $tag ?? null);
+            $quote = Quote::getAndMarkRandomQuoteText(
+                $canal->chat_id, $category ?? null
+            );
 
             $data = [
                 'chat_id' => $canal->chat_id,
